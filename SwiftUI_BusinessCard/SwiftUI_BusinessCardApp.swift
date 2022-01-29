eaab0d379e9be03102bdbbb097f14c41d4eda00e
Bhavya <bhavyathacker@gmail.com>
2022-01-29 16:04:54 +0530

    Initial Commit

---
+++ SwiftUI_BusinessCard/SwiftUI_BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BusinessCardApp.swift
+//  SwiftUI_BusinessCard
+//
+//  Created by Admin on 29/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
